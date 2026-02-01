fdb6fadcc7e441a1428dbcf756066a3d4c9b9784
parshurambagade <parshurambagade855@gmail.com>
2026-02-01 15:56:14 +0530

    add: nativewind setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "./global.css";
+
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to the LearnLog app!</Text>
+    <View className="flex-1 bg-white items-center justify-center">
+      <Text className="text-lg font-bold text-blue-600">Welcome to the LearnLog app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
